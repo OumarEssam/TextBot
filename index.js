c1a28da0cd709c526757a027fbb41c6a1265a932
OumarEssam <omar_essam99@yahoo.com>
2026-02-22 21:19:35 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,53 +1,52 @@
-		const { Client, GatewayIntentBits } = require('discord.js');
+const { Client, GatewayIntentBits } = require('discord.js');
 
 const client = new Client({
-    intents: [
-        GatewayIntentBits.Guilds,
-        GatewayIntentBits.GuildMessages,
-        GatewayIntentBits.MessageContent
-    ]
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent
+  ]
 });
 
-// الروم اللي البوت يسمع منه
+// listen from
 const allowedChannelId = "1473082358478405846";
 
-// الروم اللي الرسالة هتتنقل فيه
+// send to
 const targetChannelId = "1473082358478405846";
 
-client.once("clientReady", () => {
-    console.log(`✅ Logged in as ${client.user.tag}`);
+client.once("ready", () => {
+  console.log(`✅ Logged in as ${client.user.tag}`);
 });
 
 client.on("messageCreate", async (message) => {
-    if (message.author.bot) return; // يمنع رسائل البوتات
-    if (message.channel.id !== allowedChannelId) return;
-    if (!message.content.trim()) return;
+  if (message.author.bot) return;
+  if (message.channel.id !== allowedChannelId) return;
+  if (!message.content.trim()) return;
 
-    try {
-        await message.delete();
+  try {
+    await message.delete();
 
-const channel = message.guild.channels.cache.get(targetChannelId);
-if (!channel) return;
+    const channel = message.guild.channels.cache.get(targetChannelId);
+    if (!channel) return;
 
-await channel.send({
-    content: `━━━━━━━━━━━━━━━━━━━━━━━━
+    await channel.send({
+      content: `━━━━━━━━━━━━━━━━━━━━━━━━
 
 # **${message.content}**
 
 ⚬──────────✧──────────⚬
 
 **By:** <@${message.author.id}> | <@&1445835336931344505> | <@&1474962096432218288>`
+    });
 
-});
-} catch (err) {
-    console.log(err);	
-}
+  } catch (err) {
+    console.log(err);
+  }
 });
 
 if (!process.env.TOKEN) {
   console.error("❌ TOKEN is missing. Add TOKEN in Railway Variables.");
   process.exit(1);
 }
-client.login(process.env.TOKEN);
-//client.login("MTQ3NDY1MTIzMzk5MzEwMTM3Mw.GSj2fj.gBFVhru3YHZ4YXoX33i7bztZLxCuQz6Mz7yGAc");
 
+client.login(process.env.TOKEN);
